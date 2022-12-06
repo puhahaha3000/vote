19a6138c69f521f63904b16985e5af90b7c863f2
윤형식 <puhahaha3000@naver.com>
2022-12-06 18:02:33 +0900

    불필요한 요소 제거, 자바 버전 8로 낮춤

--- src/main/java/edu/global/common/Constant.java
+++ src/main/java/edu/global/common/Constant.java
@@ -2,35 +2,28 @@ package edu.global.common;
 
 public class Constant {
     public static final String CONNECT_POOL = "java:comp/env/jdbc/oracle/vote";
-    public static final String QUERY_MEMBER_LIST = """
-            SELECT M_NO, M_NAME, P_NAME, P_SCHOOL, M_JUMIN, M_CITY, TRIM(P_TEL1)||'-'||P_TEL2||'-'||P_TEL3
-            FROM TBL_MEMBER_202005 M, TBL_PARTY_202005 P
-            WHERE M.P_CODE = P.P_CODE""";
-    public static final String QUERY_INSERT_VOTE = """
-            INSERT INTO TBL_VOTE_202005
-                (V_JUMIN, V_NAME, M_NO, V_TIME, V_AREA, V_CONFIRM)
-            VALUES (?, ?, ?, ?, ?, ?)""";
-    public static final String QUERY_VOTE_LIST = """
-            SELECT
-                V_NAME AS NAME,
-                '19'||SUBSTR(V_JUMIN, 1, 2)||'년'||SUBSTR(V_JUMIN, 3, 2)||'월'||SUBSTR(V_JUMIN, 5, 2)||'일생' AS BIRTH_DATE,
-                TRUNC(MONTHS_BETWEEN(TO_DATE(SYSDATE), TO_DATE(19||SUBSTR(V_JUMIN, 1, 6))) / 12) AS AGE,
-                CASE SUBSTR(V_JUMIN, 7, 1)
-                    WHEN '1' THEN '남자'
-                    WHEN '2' THEN '여자' END AS GENDER,
-                V_TIME AS TIME,
-                V_CONFIRM AS CONFIRM
-            FROM TBL_VOTE_202005""";
-    public static final String QUERY_MEMBER_RANK = """
-            SELECT M.M_NO, M_NAME, COUNT
-            FROM TBL_MEMBER_202005 M, (
-                SELECT M_NO, COUNT(*) AS COUNT
-                FROM TBL_VOTE_202005
-                WHERE V_CONFIRM = 'Y'
-                GROUP BY M_NO) V
-            WHERE M.M_NO = V.M_NO
-            ORDER BY COUNT DESC""";
-    public static final String SELECT_FROM_TBL_VOTE_202005 = "SELECT * FROM TBL_VOTE_202005";
-    public static final String SELECT_FROM_TBL_PARTY_202005 = "SELECT * FROM TBL_PARTY_202005";
-    public static final String SELECT_FROM_TBL_MEMBER_202005 = "SELECT * FROM TBL_MEMBER_202005";
+    public static final String QUERY_MEMBER_LIST = "SELECT M_NO, M_NAME, P_NAME, P_SCHOOL, M_JUMIN, M_CITY, TRIM(P_TEL1)||'-'||P_TEL2||'-'||P_TEL3\n" +
+            "            FROM TBL_MEMBER_202005 M, TBL_PARTY_202005 P\n" +
+            "            WHERE M.P_CODE = P.P_CODE";
+    public static final String QUERY_INSERT_VOTE = "INSERT INTO TBL_VOTE_202005\n" +
+            "                (V_JUMIN, V_NAME, M_NO, V_TIME, V_AREA, V_CONFIRM)\n" +
+            "            VALUES (?, ?, ?, ?, ?, ?)";
+    public static final String QUERY_VOTE_LIST = "SELECT\n" +
+            "                V_NAME AS NAME,\n" +
+            "                '19'||SUBSTR(V_JUMIN, 1, 2)||'년'||SUBSTR(V_JUMIN, 3, 2)||'월'||SUBSTR(V_JUMIN, 5, 2)||'일생' AS BIRTH_DATE,\n" +
+            "                TRUNC(MONTHS_BETWEEN(TO_DATE(SYSDATE), TO_DATE(19||SUBSTR(V_JUMIN, 1, 6))) / 12) AS AGE,\n" +
+            "                CASE SUBSTR(V_JUMIN, 7, 1)\n" +
+            "                    WHEN '1' THEN '남자'\n" +
+            "                    WHEN '2' THEN '여자' END AS GENDER,\n" +
+            "                V_TIME AS TIME,\n" +
+            "                V_CONFIRM AS CONFIRM\n" +
+            "            FROM TBL_VOTE_202005";
+    public static final String QUERY_MEMBER_RANK = "SELECT M.M_NO, M_NAME, COUNT\n" +
+            "            FROM TBL_MEMBER_202005 M, (\n" +
+            "                SELECT M_NO, COUNT(*) AS COUNT\n" +
+            "                FROM TBL_VOTE_202005\n" +
+            "                WHERE V_CONFIRM = 'Y'\n" +
+            "                GROUP BY M_NO) V\n" +
+            "            WHERE M.M_NO = V.M_NO\n" +
+            "            ORDER BY COUNT DESC";
 }
